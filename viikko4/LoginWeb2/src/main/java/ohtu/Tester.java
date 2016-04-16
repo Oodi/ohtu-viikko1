27b43f032aadd2c471c800d06a8174d9c45f1976
Oodi <simo.auvinen@helsinki.fi>
2016-04-16 13:16:09 +0300

    Viikko neljä

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -3,29 +3,86 @@ package ohtu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
-    public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
 
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        testi1(driver);
+        testi2(driver);
+        testi3(driver);
+        testi4(driver, "uusiii");
+        testi5(driver);
+
+    }
+
+    private static void alusta(WebDriver driver) {
         driver.get("http://localhost:8090");
-        System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click(); 
-        
-        System.out.println("==");
-        
-        System.out.println( driver.getPageSource() );
+
+    }
+
+    private static void testi1(WebDriver driver) {
+        alusta(driver);
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
         element.submit();
-        
-        System.out.println("==");
-        System.out.println( driver.getPageSource() );
-        
+
+    }
+
+    private static void testi2(WebDriver driver) {
+        alusta(driver);
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("vaara");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+    }
+
+    private static void testi3(WebDriver driver) {
+        alusta(driver);
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("eeole");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("vaara");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+    }
+
+    private static void testi4(WebDriver driver, String nimi) {
+        alusta(driver);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys(nimi);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("sala1nen");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("sala1nen");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+
+    }
+    
+        private static void testi5(WebDriver driver) {
+            testi4(driver, "testiaaaa");
+            WebElement e = driver.findElement(By.linkText("continue to application mainpage"));
+            e.click();
+            e = driver.findElement(By.linkText("logout"));
+            e.click();
+
     }
 }
