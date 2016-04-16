05895685af7c4735ddeaf88d4b2c2b885935a826
Oodi <simo.auvinen@helsinki.fi>
2016-04-16 12:00:54 +0300

    Jotain vanhaa jäänyt pushaamatta

--- Viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -11,15 +11,17 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-//        Kirjanpito kirjanpito = new Kirjanpito();
-//        Varasto varasto = new Varasto(kirjanpito);
-//        Pankki pankki = new Pankki(kirjanpito);
-//        Viitegeneraattori viitegen = new Viitegeneraattori();
-//        Kauppa kauppa = new Kauppa(varasto, pankki, viitegen);
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
-
         Kauppa kauppa = ctx.getBean(Kauppa.class);
+        alustaAsiakas1(kauppa);
+        alustaAsiakas2(kauppa);
+        // kirjanpito
+        for (String tapahtuma : ctx.getBean(Kirjanpito.class).getTapahtumat()) {
+            System.out.println(tapahtuma);
+        }
+    }
 
+    private static void alustaAsiakas1(Kauppa kauppa) {
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
@@ -27,18 +29,14 @@ public class Main {
         kauppa.lisaaKoriin(3);
         kauppa.poistaKorista(1);
         kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
+    }
 
+    private static void alustaAsiakas2(Kauppa kauppa) {
         // seuraava asiakas
         kauppa.aloitaAsiointi();
         for (int i = 0; i < 24; i++) {
             kauppa.lisaaKoriin(5);
         }
-
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
-
-        // kirjanpito
-        for (String tapahtuma : ctx.getBean(Kirjanpito.class).getTapahtumat()) {
-            System.out.println(tapahtuma);
-        }
     }
 }
--- Viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ Viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,4 +1,4 @@
- package ohtu.verkkokauppa;
+package ohtu.verkkokauppa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
